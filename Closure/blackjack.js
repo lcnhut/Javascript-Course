e086195b537754886f1393a898319387e3ad58d6
lcnhut <nhut.le@nfq.asia>
2022-05-26 10:26:27 +0700

    New: blackjack game

---
+++ Closure/blackjack.js
@@ -0,0 +1,56 @@
+const blackjack = (array) => {
+  let currentPoint = 0;
+
+  return (a, b) => {
+    currentPoint = a + b;
+    let isBust = false;
+    let lastPoint = currentPoint;
+    return () => {
+      let lastPoint = currentPoint;
+      if (!isBust) {
+        if (currentPoint <= 21) {
+          currentPoint += array.shift();
+          return lastPoint;
+        } else if (currentPoint > 21) {
+          isBust = true;
+          return "Bust";
+        }
+      } else if (currentPoint > 21) {
+        return "You are done!";
+      }
+    };
+  };
+};
+
+const deal = blackjack([
+  2, 6, 1, 7, 11, 4, 6, 3, 9, 8, 9, 3, 10, 4, 5, 3, 7, 4, 9, 6, 10, 11,
+]);
+
+// /*** PLAYER 1 ***/
+const i_like_to_live_dangerously = deal(4, 5);
+console.log(i_like_to_live_dangerously()); // => should log 9
+console.log(i_like_to_live_dangerously()); // => should log 11
+console.log(i_like_to_live_dangerously()); // => should log 17
+console.log(i_like_to_live_dangerously()); // => should log 18
+console.log(i_like_to_live_dangerously()); // => should log 'bust'
+console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+
+/*** BELOW LINES ARE FOR THE BONUS ***/
+
+/*** PLAYER 2 ***/
+const i_TOO_like_to_live_dangerously = deal(2, 2);
+console.log(i_TOO_like_to_live_dangerously()); // => should log 4
+console.log(i_TOO_like_to_live_dangerously()); // => should log 15
+console.log(i_TOO_like_to_live_dangerously()); // => should log 19
+console.log(i_TOO_like_to_live_dangerously()); // => should log 'bust'
+console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+
+// /*** PLAYER 3 ***/
+const i_ALSO_like_to_live_dangerously = deal(3, 7);
+console.log(i_ALSO_like_to_live_dangerously()); // => should log 10
+console.log(i_ALSO_like_to_live_dangerously()); // => should log 13
+console.log(i_ALSO_like_to_live_dangerously()); // => should log 'bust'
+console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
+console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
